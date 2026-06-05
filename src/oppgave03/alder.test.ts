6e34004e617023872ccedc5864fe9e52b0395efe
Your Name <your.email@example.com>
2026-06-05 13:20:48 +0200

    Completed Oppgave 3

--- src/oppgave03/alder.test.ts
+++ src/oppgave03/alder.test.ts
@@ -1,16 +1,16 @@
-import {describe, expect, test} from 'vitest';
-import {alderIAr} from './alder';
+import { describe, expect, test } from 'vitest';
+import { alder } from './alder';
 
 describe('Oppgave 3 — Beregn alder', () => {
   test('født 1990', () => {
-    expect(alderIAr(1990, 2026)).toBe(36);
+    expect(alder(1990, 2026)).toBe(36);
   });
 
   test('født 2000', () => {
-    expect(alderIAr(2000, 2026)).toBe(26);
+    expect(alder(2000, 2026)).toBe(26);
   });
 
   test('samme år gir null', () => {
-    expect(alderIAr(2026, 2026)).toBe(0);
+    expect(alder(2026, 2026)).toBe(0);
   });
 });
